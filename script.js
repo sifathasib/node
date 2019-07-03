d26d1bbb3c578c3425f5249fece8e6d20b97c0a7
sifat <sifathasib@gmail.com>
2019-07-03 21:01:08 +0600

    rectangle and package json

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-var msg ='';
+var rect ={
+    perimeter : (x,y)=>(2*(x+y)),
+    area : (x,y)=>(2*(x+y)),
+}
 
-console.log(global.msg);
+function solveRect(l,b){
+    console.log("solving rectangle for "+l+" and "+b+" is: ")
+    if (l<= 0 || b <=0){
+       console.log("dimensions should be greater than 0"); 
+    }
+    else{
+        console.log("area is: "+rect.area(l,b)+"\nperimeter is : "+rect.perimeter(l,b));
+    }
+
+}
+
+solveRect(10,5);
+solveRect(5,6);
+solveRect(0,19);
